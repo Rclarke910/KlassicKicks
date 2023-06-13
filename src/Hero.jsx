1683e226ef7084cb96f50b94a52b7ba5cfd4ea61
Rashawn Clarke <102108419+Rclarke910@users.noreply.github.com>
2023-06-13 11:35:48 -0400

    hero font size adjustment

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero(){
     return (
         <section id = "hero">
-          <h4 style={{ fontSize: '70px'}}>Buy And Sell</h4>
+          <h3>Buy And Sell</h3>
           <h2>The Best Deals</h2>
           <h1>On Vintage Sneakers</h1>
           <p>Save More With Coupons And Up To 70% Off!</p>
